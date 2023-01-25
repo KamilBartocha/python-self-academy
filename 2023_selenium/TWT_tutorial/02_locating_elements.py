b2163ab91cd7d9590779ecaf3b1c81f9188bfd26
Kamil Bartocha <kamilbartocha53@gmail.com>
2023-01-25 09:22:50 +0100

    02 task with solution

--- 2023_selenium/TWT_tutorial/02_locating_elements.py
+++ 2023_selenium/TWT_tutorial/02_locating_elements.py
@@ -1,7 +1,31 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
-driver = webdriver.Chrome()
-driver.get("https://justjoin.it")
+# search order: id -> class -> name
+# Task: search "test" in search bar and print summary of all results from
+# website https://techwithtim.net.
 
+driver = webdriver.Chrome()
+driver.get("https://techwithtim.net")
+print(driver.title)
+
+search = driver.find_element(By.NAME, "s")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
+    articles = main.find_elements(By.TAG_NAME, "article")
+    for article in articles:
+        header = article.find_element(By.CLASS_NAME, "entry-summary")
+        print(header.text)
+finally:
+    driver.quit()
