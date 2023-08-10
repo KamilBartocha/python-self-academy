da207b34cac2bb6a31c0011631a10ffecb4a7749
Kamil Bartocha <kamilbartocha53@gmail.com>
2023-08-10 09:43:46 +0200

    fix

--- 2023_pytest/03_cb_example_tdd/calculator.py
+++ 2023_pytest/03_cb_example_tdd/calculator.py
@@ -1,10 +1,10 @@
 def calculator(num1, num2, operator):
     if operator == "+":
-        result = num1 + num2
+        result = num1 + num2 + 1
         return result
     elif operator == "-":
         return num1 - num2
 
 
 
-print(calculator(2,3,"+"))
+print(calculator(2, 3, "-"))
--- 2023_pytest/03_cb_example_tdd/test_calculator.py
+++ 2023_pytest/03_cb_example_tdd/test_calculator.py
@@ -8,9 +8,7 @@ def test_add_two_numbers():
     assert calculator(number1, number2, operator) == 8
 
 def test_sub_two_numbers():
-    number1 = 1-1000000
+    number1 = 6
     number2 = 2
     operator = "-"
     assert calculator(number1, number2, operator) == 4
-
-
